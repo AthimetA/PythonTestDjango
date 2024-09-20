d6fe9c2c0f58a7c0f94282327e64e381a19b3f4d
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2024-09-20 20:43:54 +0700

    clean and create new for backup

--- pythontest/employeemanagement_apk/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from employeemanagement_apk.models import Employee, Department, Status, Position
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Department)
-admin.site.register(Status)
-admin.site.register(Position)
--- pythontest/pythontest/wsgi.py
+++ pythontest/pythontest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythontest.production_setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythontest.settings')
 
 application = get_wsgi_application()

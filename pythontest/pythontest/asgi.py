92203dc5d43392eefb2e0a73d2153105c762deff
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2024-09-22 00:49:08 +0700

    clean

--- pythontest/pythontest/asgi.py
+++ pythontest/pythontest/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythontest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythontest.development_setting')
 
 application = get_asgi_application()
